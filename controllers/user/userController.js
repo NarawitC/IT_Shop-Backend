ad8513f8f897da30b954ca3dd8bac39f5a6c7689
NarawitC <narawit.developer@outlook.com>
2022-06-11 22:59:27 +0700

    fix(updateUserInfo): hashedPassword

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,4 +1,5 @@
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const { CONFIRMED } = require('../../config/constants');
 const { User, Order, OrderItem, Product } = require('../../models/index');
@@ -85,7 +86,6 @@ exports.getUserOrderItemsFromOrderId = async (req, res, next) => {
 exports.updateUserInfo = async (req, res, next) => {
   try {
     const input = req.body;
-
     if (input.email) {
       const isEmail = validator.isEmail('' + input.email);
       if (!isEmail) {
@@ -93,11 +93,14 @@ exports.updateUserInfo = async (req, res, next) => {
       }
     }
 
-    if (input.password) {
+    if (input.password || input.confirmPassword) {
       if (input.password !== input.confirmPassword) {
         createError('Password did not match', 400);
       }
     }
+    const hashedPassword = await bcryptjs.hash(input.password, 12);
+    input.password = hashedPassword;
+
     if (input.phoneNumber) {
       const isPhoneNumber = validator.isMobilePhone(
         '' + input.phoneNumber,
@@ -107,13 +110,12 @@ exports.updateUserInfo = async (req, res, next) => {
         createError('PhoneNumber is invalid format', 400);
       }
     }
-
-    const user = await User.update(input, {
+    const updated = await User.update(input, {
       where: { id: req.user.id },
     });
     //! ถ้าจะใช้ของจากการ update ให้ findOneมาใหม่ เพราะupdate return จำนวน recordที่ทำการ update
 
-    if (!user) {
+    if (!updated) {
       createError('You are unauthorized', 404);
     }
     res.status(200).json({ message: 'User updated' });
